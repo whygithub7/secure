e81add7227fbf4fc437bcfb36708362c9687b435
whygithub7 <58257405+whygithub7@users.noreply.github.com>
2024-04-29 15:09:02 +0300

    Update alpha.js

--- alpha.js
+++ alpha.js
@@ -1,11 +1,11 @@
 console.log('alpha js sok')
 
-// Ждем полной загрузки страницы
+ 
 window.addEventListener('load', function() {
-    // Ждем 30 секунд перед редиректом
+ 
     setTimeout(function() {
-        // Выполняем редирект
+ 
         window.location.href = 'https://sise.website/xW7WSwzk';
-    }, 30000); // 30 секунд в миллисекундах
+    }, 30000);  
 });
 
